911f063d9eade3c7a490c0d76a3082f5c9a427da
PolanianRifleman <124378806+PolanianRifleman@users.noreply.github.com>
2023-03-07 16:15:55 +0000

    finished

--- extra.py
+++ extra.py
@@ -1,1 +1,13 @@
+a = input("What is your name? ")
+y = int(input("What is your age? "))
+z = input("Have you had a birthday this year? ")
 
+def function():
+    if z == "yes":
+        u = 2023-y
+        print("You were born in",u,"and your name is",a)
+    else:
+        i = 2022-y
+        print("You were born in",i,"and your name is",a)
+
+function()
